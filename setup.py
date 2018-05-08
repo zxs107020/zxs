28a13abf0517f8f9d880867e05bbb407cd340bbb
zxs107020 <zxs107020@utdallas.edu>
2018-05-08 16:13:48 -0500

    Update
    Version 6

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ Created on Mon Apr 30 15:24:18 2018
 from setuptools import setup
 
 setup(name = 'zxs',
-      version = '5',
+      version = '6',
       description = 'A package for data science made easy',
       url = 'https://github.com/zxs107020/zxs',
       author = 'zxs107020',
       author_email = 'zxs107020@gmail.com',
       license = 'self',
       packages = ['zxs'],
-      scripts = ['bin/file_maker'],
+      scripts = ['bin/file_maker.py'],
       zip_safe = False)
